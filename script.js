3f794fa1e40f972ea4ab2e260d2aeae2830a5b69
Haroon Frotan <haroon_fro@hotmail.com>
2023-09-26 16:09:47 -0400

    score is good

--- script.js
+++ script.js
@@ -15,41 +15,52 @@ const hulk = document.querySelector('#hulk');
 const tank = document.querySelector('#tank');
 const score = document.querySelector('#score');
 
-
-
-
-
 //jump function here
-
-
 function jump() {
-console.log('jump working')
+// console.log('jump working')
     hulk.classList.add('jump-animation');
     setTimeout(() => {
         hulk.classList.remove('jump-animation')
-    }, 900);
+    }, 800);
 
 }
+//hulk jump complete
+document.addEventListener('click', () => {
+    jump();
+});
+
+//score and collision here
+
+setInterval(() => {
+
+    // score.innerHTML++;
+    // console.log('score working')
+
+    const hulkTopPosition = parseInt(window.getComputedStyle(hulk)
+         .getPropertyValue('top'));
+ 
+    const tankLeftPosition = parseInt(window.getComputedStyle(tank)
+         .getPropertyValue('left'));
+
+    // console.log(tankLeftPosition, hulkTopPosition)
+
+    //show tank only when value is positive 
+
+    if (tankLeftPosition < 0) {
+        tank.style.display = 'none';
+    } else {
+        tank.style.display = '';
+    } 
+
+});
+
+
+
+
+
 
 //hulk grunt function
 
-function grunt () {
+// document.getElementById('grunt');
+// play();
   
-}
-
-
-//hulk jump complete
-
-
-document.addEventListener('click', () => {
- console.log('click working')       
-    jump();
-    
-});
-
-// console.log("working");
-
-
-
-
-// score here
